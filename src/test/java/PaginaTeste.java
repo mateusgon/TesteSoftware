ce3078651dc646355d13cd60fbd0867e58f46f64
ice <ice@lab06m17.nrc.ice.ufjf.br>
2018-05-04 19:37:48 -0300

    teste

--- src/test/java/PaginaTeste.java
+++ src/test/java/PaginaTeste.java
@@ -1,11 +1,11 @@
+
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.PageFactory;
 
-import static org.junit.Assert.assertNotNull;
+
 
 public class PaginaTeste {
 
@@ -14,7 +14,7 @@ public class PaginaTeste {
     @BeforeClass
     public static void configura()
     {
-        System.setProperty("webdriver.gecko.driver", "C:\\Users\\Mateus G\\Desktop\\geckodriver.exe");
+        System.setProperty("webdriver.gecko.driver", "/ice/Downloads/geckodriver");
         driver = (WebDriver) new FirefoxDriver();
         driver.get("https://accounts.google.com/signup/v2/webcreateaccount?service=mail&continue=https%3A%2F%2Fmail.google.com%2Fmail%2F&ltmpl=default&flowName=GlifWebSignIn&flowEntry=SignUp");
     }
