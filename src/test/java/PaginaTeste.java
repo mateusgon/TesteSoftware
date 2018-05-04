838ddb5b4be2642e72bdf4e23195bc8ca9c2ef63
Mateus G <Mateus G@172.18.138.41>
2018-05-04 20:04:27 -0300

    Atualizando

--- src/test/java/PaginaTeste.java
+++ src/test/java/PaginaTeste.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.PageFactory;
 
-
+//
 
 public class PaginaTeste {
 
@@ -14,7 +14,7 @@ public class PaginaTeste {
     @BeforeClass
     public static void configura()
     {
-        System.setProperty("webdriver.gecko.driver", "/ice/Downloads/geckodriver");
+        System.setProperty("webdriver.gecko.driver", "C:\\Users\\Mateus G\\Desktop\\geckodriver.exe");
         driver = (WebDriver) new FirefoxDriver();
         driver.get("https://accounts.google.com/signup/v2/webcreateaccount?service=mail&continue=https%3A%2F%2Fmail.google.com%2Fmail%2F&ltmpl=default&flowName=GlifWebSignIn&flowEntry=SignUp");
     }
